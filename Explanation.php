780f4a2003198d9dd1eb572d3f77c7621ab1500f
TimothyCobussen <36793977+TimothyCobussen@users.noreply.github.com>
2018-03-06 21:27:56 +0100

    halve aanpassing uitleg

--- Explanation.php
+++ Explanation.php
@@ -2,12 +2,13 @@
 	include_once "include/pageDefaults.php";
 	getPageStart();
 ?>
-
-
-
-
-
-
+<div>
+	<div class="addition_explanation">
+		
+		
+		
+	</div>
+</div>
 
 <?php
 	getPageEnd();
--- index.php
+++ index.php
@@ -11,7 +11,6 @@
 	</div>
 </div>
 
-
 <?php
     getPageEnd();
 ?>
