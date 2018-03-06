9ba6dcc626924f56de34b8c1d937bb3f4bc727f0
blmkroez <blmkroez@avans.nl>
2018-03-06 19:50:59 +0100

    more styling

--- question_select.php
+++ question_select.php
@@ -3,56 +3,72 @@ include_once "include/pageDefaults.php";
 getPageStart();
 ?>
     <div>
-        <form action="questions.php" method="get">
-            <div>
+        <form action="questions.php" method="get" class="question_selector">
+            <div class="difficulty_selector">
                 <h2>Selecteer de moeilijkheidsgraad</h2>
                 <div>
                     <div>
-                        <input type="radio" name="difficulty" value="0" required>
-                        Makkelijk
+                        <label><input type="radio" name="difficulty" value="0" required>Makkelijk</label>
                     </div>
                     <div>
-                        <input type="radio" name="difficulty" value="1" required>
-                        Gemiddeld
+                        <label><input type="radio" name="difficulty" value="1" required>Gemiddeld</label>
                     </div>
                     <div>
-                        <input type="radio" name="difficulty" value="2" required>
-                        Moelijk
+                        <label><input type="radio" name="difficulty" value="2" required>Moelijk</label>
                     </div>
                 </div>
             </div>
 
-            <div>
+            <div class="question_type_selector">
                 <h2>Selecteer het type som</h2>
                 <div>
-                    <div>
-                        <input type="radio" name='questionType' value="1" required>
-                        +
-                    </div><div>
-                        <input type="radio" name='questionType' value="2" required>
-                        -
-                    </div><div>
-                        <input type="radio" name='questionType' value="3" required>
-                        *
-                    </div><div>
-                        <input type="radio" name='questionType' value="4" required>
-                        :
-                    </div><div>
-						<input type="radio" name='questionType' value="5" required>
-						²
-					</div><div>
-						<input type="radio" name='questionType' value="6" required>
-                        √
-					</div><div>
-                        <input type="radio" name='questionType' value="0" required>
-                        willekeurig
-                    </div>
+                    <label>
+                        <div>
+                            <input type="radio" name='questionType' value="1" required>
+                            +
+                        </div>
+                    </label>
+                    <label>
+                        <div>
+                            <input type="radio" name='questionType' value="2" required>
+                            -
+                        </div>
+                    </label>
+                    <label>
+                        <div>
+                            <input type="radio" name='questionType' value="3" required>
+                            *
+                        </div>
+                    </label>
+                    <label>
+                        <div>
+                            <input type="radio" name='questionType' value="4" required>
+                            :
+                        </div>
+                    </label>
+                    <label>
+                        <div>
+                            <input type="radio" name='questionType' value="5" required>
+                            ²
+                        </div>
+                    </label>
+                    <label>
+                        <div>
+                            <input type="radio" name='questionType' value="6" required>
+                            √
+                        </div>
+                    </label>
+                    <label>
+                        <div>
+                            <input type="radio" name='questionType' value="0" required>
+                            willekeurig
+                        </div>
+                    </label>
                 </div>
-
-                <div>
-                    <h2>Hoeveel vragen?</h2>
-                    <input type="number" name="numQuestions">
-                </div>
+            </div>
+            <div class="question_amount">
+                <h2>Hoeveel vragen?</h2>
+                <input type="number" name="numQuestions">
             </div>
             <button type="submit">GA!</button>
         </form>
